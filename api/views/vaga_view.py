1cdcf2f8b6cc5a576b9643e07a47d5971d343784
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-07-08 10:05:57 -0300

    07.02_Configurando_Paginacao

--- api/views/vaga_view.py
+++ api/views/vaga_view.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from ..services import vaga_service
@@ -8,9 +9,12 @@ from ..entidades import vaga
 
 class VagaList(APIView):
     def get(self, request, format=None):
+        paginacao = PageNumberPagination()
         vagas = vaga_service.listar_vagas()
-        serializer = vaga_serializer.VagaSerializer(vagas, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        resultado = paginacao.paginate_queryset(vagas, request)
+        serializer = vaga_serializer.VagaSerializer(resultado, many=True)
+        return paginacao.get_paginated_response(serializer.data)
+        #return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
         serializer = vaga_serializer.VagaSerializer(data=request.data)
