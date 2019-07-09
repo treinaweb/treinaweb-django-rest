da8b5a94fef2c521958e9cb5b4802d3968963be3
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-07-09 09:42:09 -0300

    07.003_Personalizando_Paginacao

--- api/pagination.py
+++ api/pagination.py
@@ -1,7 +1,17 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class PaginacaoCustomizada(PageNumberPagination):
     page_size = 3
     page_size_query_param = 'tamanho_pagina'
     max_page_size = 5
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'results': data
+        })
