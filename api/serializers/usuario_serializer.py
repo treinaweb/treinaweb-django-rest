8720a26eda8139a7b0667850fc870e32c2ac5234
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-07-09 11:00:36 -0300

    08.02_Cadastrando_Usuarios

---
+++ api/serializers/usuario_serializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    def create(self, validated_data):
+        user = super(UsuarioSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import tecnologia_view, vaga_view
+from .views import tecnologia_view, vaga_view, usuario_view
 
 urlpatterns = [
     path('tecnologias/', tecnologia_view.TecnologiaList.as_view(), name='tecnologia-list'),
     path('tecnologias/<int:id>', tecnologia_view.TecnologiaDetalhes.as_view(), name='tecnologia-detalhes'),
     path('vagas/', vaga_view.VagaList.as_view(), name='vaga-list'),
     path('vagas/<int:id>', vaga_view.VagaDetalhes.as_view(), name='vaga-detalhes'),
+    path('usuarios/', usuario_view.UsuarioList.as_view(), name='usuario-list'),
 ]
